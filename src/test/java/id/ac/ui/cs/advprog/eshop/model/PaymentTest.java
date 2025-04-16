425b9eebf0af7de47a3957f36c54677458eb524b
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2025-04-17 01:57:58 +0700

    [REFACTOR] refactor of paymenttest.java

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -8,15 +8,24 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentTest {
+
     @Test
-    void testPaymentConstructor() {
+    void shouldCreatePaymentWithAllFields() {
         Map<String, String> data = new HashMap<>();
         data.put("voucherCode", "ESHOP1234ABCD5678");
-        Payment payment = new Payment("1", "Voucher", "SUCCESS", data);
 
-        assertEquals("1", payment.getId());
-        assertEquals("Voucher", payment.getMethod());
-        assertEquals("SUCCESS", payment.getStatus());
-        assertEquals("ESHOP1234ABCD5678", payment.getPaymentData().get("voucherCode"));
+        Payment payment = new Payment(
+                "1",
+                "Voucher",
+                "SUCCESS",
+                data
+        );
+
+        assertAll(
+                () -> assertEquals("1", payment.getId()),
+                () -> assertEquals("Voucher", payment.getMethod()),
+                () -> assertEquals("SUCCESS", payment.getStatus()),
+                () -> assertEquals("ESHOP1234ABCD5678", payment.getPaymentData().get("voucherCode"))
+        );
     }
 }
